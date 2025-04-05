cd0de38643b1869ce7a97342b8896171a619ecd9
Ema Teodora Nicolae <emilia.nicolae16@gmail.com>
2025-04-05 16:56:47 +0300

    Mini Game 1

---
+++ Assets/ScriptsFlappyBat/GameManager.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Player player;
+    public GameObject playButton;
+    public GameObject gameOver;
+
+    public GameObject done;
+    public int maxScore = 5;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+        Pause();
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        player.enabled = false;
+        done.SetActive(true);
+    }
+
+    public void Play()
+    {
+        playButton.SetActive(false);
+        gameOver.SetActive(false);
+        done.SetActive(false);
+        Time.timeScale = 1f;
+        player.enabled = true;
+
+        foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Obstacle"))
+        {
+            Destroy(obj);
+        }
+    }
+
+    public void GameOver()
+    {
+        playButton.SetActive(true);
+        gameOver.SetActive(true);
+        Pause();
+    }
+
+}
+
+
